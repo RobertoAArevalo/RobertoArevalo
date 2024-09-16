82519cc86b20e2e1bcab2f862d8e26a52003d99f
Roberto Arevalo <roberttt44@hotmail.com>
2024-09-15 17:49:45 -0700

    Home page development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Roberto Arevalo</h1>
+        <nav className = "nav-tabs">
+          <a href="#home" className ="tab-link">Home</a>
+          <a href="#classesApp" className ="tab-link">Classes App</a>
+          <a href="#JavaApp" className ="tab-link">Java App</a>
+          <a href="#Coursework" className ="tab-link">Coursework</a>
+          <a href="#contact" className ="tab-link">Contact</a>
+        </nav>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="github_site"
+          href="https://github.com/RobertoAArevalo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
+      {/*Main content*/}
+      <main className="App-body">
+        <section>
+          <h2>About Me</h2>
+        <p>
+          California State University Monterey Bay <br />
+          B.S. Computer Science<br />
+          I am a graduate from California State University - Monterey Bay.<br />
+          I am eager to leverage my expertise and pursue new challenges in the technology industry<br />
+          I am pursuing a profession in software engineering as a full stack engineer or product manager.<br />
+          I enjoy developing websites and application on my spare time.<br />
+        </p>
+        </section>
+      </main>
     </div>
   );
 }
